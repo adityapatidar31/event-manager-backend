7db61fe6abab62ed8afe09880c9e91375bdf2551
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-11 04:36:23 +0530

    setup env

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
@@ -10,7 +10,6 @@ const app = require("./app");
 // );
 
 // mongoose.connect(DB).then(() => console.log("DB connection successful!"));
-
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
