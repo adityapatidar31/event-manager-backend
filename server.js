e28d9c97d78a35b809ae3eca5f79670bfc734cee
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-11 04:45:35 +0530

    connect db with mongoose

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-// const DB = process.env.DATABASE.replace(
-//   "<PASSWORD>",
-//   process.env.DATABASE_PASSWORD
-// );
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-// mongoose.connect(DB).then(() => console.log("DB connection successful!"));
+mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
