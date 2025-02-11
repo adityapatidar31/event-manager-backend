8483988f7da89eb982994faaaeb3070a7d767673
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-11 11:54:28 +0530

    add more property to the eventModel

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,14 +1,33 @@
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true, trim: true },
   description: { type: String, required: true },
   category: {
     type: String,
     enum: ["Tech", "Music", "Sports", "Education"],
     required: true,
   },
-  date: { type: Date, required: true },
+  capacity: {
+    type: Number,
+    required: true,
+    min: [1, "capacity must be above 1"],
+  },
+  duration: {
+    type: Number,
+    required: true,
+    min: [1, "duration must be above than 1 minute"],
+  },
+  date: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: function (date) {
+        return date >= Date.now();
+      },
+      message: "Invalid date you can not create event before now",
+    },
+  },
   location: { type: String, required: true },
   image: { type: String }, // Cloudinary URL
   attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Registered users
