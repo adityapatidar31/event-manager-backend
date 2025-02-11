579f024d847139c6689a3a439cf7999186ab8ab3
Aditya Patidar <adityapatidar.2020@gmail.com>
2025-02-11 05:50:28 +0530

    add send of jwt token with the response of signin and login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,45 @@
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+
+function signToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    sameSite: "none",
+  };
+
+  res.cookie("eventjwtcookie", token, cookieOptions);
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
 
 exports.signUp = async (req, res) => {
   const { name, email, password, passwordConfirm, photo } = req.body;
   const userData = { name, email, password, passwordConfirm };
   try {
     const newUser = await User.create(userData);
-    res.status(201).json({ status: "Success", data: { user: newUser } });
+    createSendToken(newUser, 201, res);
   } catch (e) {
     console.log(e);
     res.status(500).json({ status: "failed", error: "User Creation failed" });
-
     return;
   }
 };
@@ -39,4 +69,5 @@ exports.login = async (req, res) => {
     status: "success",
     data: user,
   });
+  createSendToken(user, 200, res);
 };
